0ab5c61ed326f688b4737305331c65b555c4c02e
derek hecker <derekhecker8@gmail.com>
2020-09-09 18:47:38 -0400

    Created Initial AppUser Class and IAppUser Interface

---
+++ P3 Code/P3 Code/IAppUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_Code
+{
+    public interface IAppUser
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void setAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
