28fcaf8f8256df990816ab00b267767bbc3719c8
derek hecker <derekhecker8@gmail.com>
2020-09-09 18:33:55 -0400

    Initial Solution Creation

---
+++ P3 Code/P3 Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
